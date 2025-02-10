4db3a16f6c1d22286839c20218fbc2b83acd9036
yadatubtes <yada.tu@ku.th>
2025-02-10 16:48:19 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,68 @@
-function submitData () {
-    let firstNameDOM = document.querySelector('input[name=firstname]');
-    let lastNameDOM = document.querySelector('input[name=lastname]');
-    let ageDOM = document.querySelector('input[name=age]');
-    let genderDOM = document.querySelector('input[name=gender]:checked');
-    let interestDOMs = document.querySelectorAll('input[name=interest]:checked');
-    let descriptionDOM = document.querySelector('textarea[name=description]');
-    let interest = ''
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-    for (let i = 0; i < interestDOMs.length; i++) {
-        interest += interestDOMs[i].value
-        if (i != interestDOMs.length - 1) {
-            interest += ','
-        }
+app.use(bodyParser.json());
+const port = 8000;
+
+// เก็บ user
+let users = []
+let counter = 1
+
+// path = get /users
+app.get('/users', (req, res) => {
+    res.json(users);
+})
+
+// path = POST / user
+app.post('/user', (req, res) => {
+    let user = req.body;
+    user.id = counter
+    counter += 1
+    users.push(user);
+    res.json({
+        message: "User created",
+        user: user
+    });
+})
+
+//path = put /user/ : id
+app.put('/user/:id', (req, res) => {
+    let.id = req.params.id;
+    let updateUser = req.body;
+    // หา index ของ user ที่ต้องการ update
+    let selectedIndex = users.findIndex(user => user.id == id)
+    //  update user
+    if (updateUser.firstname) {
+        users[selectedIndex].firstname = updateUser.firstname
     }
 
-
-    let userData = {
-        firstName: firstNameDOM.value,
-        lastName: lastNameDOM.value,
-        age: ageDOM.value,
-        gender: genderDOM.value,
-        description: descriptionDOM.value,
-        interest: interest
+    if (updateUser.firstname) {
+        users[selectedIndex].lastname = updateUser.lastname
     }
-     console.log('submitData', userData);
-}
+
+    
+
+    res.json({
+        message: "User updated",
+        data: {
+            user: updateUser,
+            indexUpdate: selectedIndex
+      }
+    });
+})
+
+app.delete('/user/:id', (req, res) => {
+    let id = req.params.id;
+    let selectedIndex = users.findIndex(user => user.id == id)
+    
+    users.splice(selectedIndex, 1)
+    res.json({
+        message: "User deleted",
+        indexDeleted: selectedIndex
+    });
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
